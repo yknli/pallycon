faf6e21beec4b5c1e57113538fbb831ee6081829
yknli <augustus0613@hotmail.com>
2024-08-03 00:00:14 +0800

    feat: Define configurations of pallycon site credentials

--- lib/pallycon/configuration.rb
+++ lib/pallycon/configuration.rb
@@ -1,4 +1,30 @@
 module Pallycon
   class Configuration
+    class SiteIDNotSet < StandardError; end
+    class SiteKeyNotSet < StandardError; end
+    class SiteAccessKeyNotSet < StandardError; end
+
+    attr_accessor :site_id
+    attr_accessor :site_key
+    attr_accessor :site_access_key
+
+    def site_id=(site_id)
+      raise SiteIDNotSet, "Please set PALLYCON site id." if site_id.to_s.strip.empty?
+
+      @site_id = site_id
+    end
+
+    def site_key=(site_key)
+      raise SiteKeyNotSet, "Please set PALLYCON site key." if site_key.to_s.strip.empty?
+
+      @site_key = site_key
+    end
+
+    def site_access_key=(site_access_key)
+      raise SiteAccessKeyNotSet, "Please set PALLYCON site access key." if site_access_key.to_s.strip.empty?
+
+      @site_access_key = site_access_key
+    end
+
   end
 end
