d00d22966b5974cda3391fa94d6689a275d9c9f8
yknli <augustus0613@hotmail.com>
2024-08-03 01:39:31 +0800

    feat: Expose the configuration

--- lib/pallycon.rb
+++ lib/pallycon.rb
@@ -1,8 +1,24 @@
 # frozen_string_literal: true
 
 require_relative "pallycon/version"
+require_relative "pallycon/configuration"
 
 module Pallycon
   class Error < StandardError; end
 
+  class << self
+    def configuration
+      @configuration ||= Configuration.new
+    end
+
+    # Set configurations in your application:
+    # Pallycon.configure do |config|
+    #   config.site_id = "<your_site_id>"
+    #   config.site_key = "<your_site_key>"
+    #   config.site_access_key = "<your_site_access_key>"
+    # end
+    def configure
+      yield(configuration)
+    end
+  end
 end
